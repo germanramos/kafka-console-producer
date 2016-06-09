162ded9f029d9c3997ffc35a48a40c0f1b2ca9e7
German Ramos Garcia <german.ramos.garcia@bbva.com>
2016-06-09 16:24:45 +0200

    doc: update readme

--- README.md
+++ README.md
@@ -26,7 +26,7 @@ Use environment variables
 
 - KAFKA_SERVICE, "kafka", The DNS name for input Kafka broker service
 - KAFKA_PORT, "9092", Port to connect to input Kafka peers
-- TOPIC, "kafka-console-producer", The topic to consume
+- TOPIC, "mytopic", The topic to consume
 - VERBOSE, "false, Set to `true` if you want verbose output
 
 ## Download
--- gopath/src/service/main.go
+++ gopath/src/service/main.go
@@ -8,10 +8,10 @@ import (
 func main() {
 	log.SetOutput(os.Stderr)
 	var (
-		kafkaService = getConfig("KAFKA_SERVICE", "kafka")          // The DNS name for input Kafka broker service
-		kafkaPort    = getConfig("KAFKA_PORT", "9092")              // Port to connect to input Kafka peers
-		topic        = getConfig("TOPIC", "kafka-console-producer") // The topic to consume
-		verbose      = getConfig("VERBOSE", "false")                // Set to `true` if you want to turn on sarama logging
+		kafkaService = getConfig("KAFKA_SERVICE", "kafka") // The DNS name for input Kafka broker service
+		kafkaPort    = getConfig("KAFKA_PORT", "9092")     // Port to connect to input Kafka peers
+		topic        = getConfig("TOPIC", "mytopic")       // The topic to consume
+		verbose      = getConfig("VERBOSE", "false")       // Set to `true` if you want to turn on sarama logging
 	)
 	producer(kafkaService, kafkaPort, topic, verbose == "true")
 }
