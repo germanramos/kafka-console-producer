ed5c43ce1a900cace4ad75f85fc2dea4c3a739d9
German Ramos Garcia <german.ramos.garcia@bbva.com>
2016-06-24 12:13:23 +0200

    feat: wait before close sarama producer

--- gopath/src/service/main.go
+++ gopath/src/service/main.go
@@ -4,16 +4,19 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"strconv"
+	"time"
 )
 
 func main() {
 	log.SetOutput(os.Stderr)
-	log.Print("kafka-console-producer v0.5.0")
+	log.Print("kafka-console-producer v0.5.1")
 	var (
-		kafkaService = getConfig("KAFKA_SERVICE", "kafka") // The DNS name for input Kafka broker service
-		kafkaPort    = getConfig("KAFKA_PORT", "9092")     // Port to connect to input Kafka peers
-		topic        = getConfig("TOPIC", "mytopic")       // The topic to consume
-		verbose      = getConfig("VERBOSE", "false")       // Set to `true` if you want to turn on sarama logging
+		kafkaService  = getConfig("KAFKA_SERVICE", "kafka") // The DNS name for input Kafka broker service
+		kafkaPort     = getConfig("KAFKA_PORT", "9092")     // Port to connect to input Kafka peers
+		topic         = getConfig("TOPIC", "mytopic")       // The topic to consume
+		verbose       = getConfig("VERBOSE", "false")       // Set to `true` if you want to turn on sarama logging
+		finishTimeout = getConfig("FINISH_TIMEOUT", "1")    // Number of seconds to wait for exit after end of line is received
 	)
 	messages := make(chan string)
 	saramaProducer := producer(kafkaService, kafkaPort, topic, messages, verbose == "true")
@@ -26,6 +29,8 @@ func main() {
 			log.Print("Discarting empty line")
 		}
 	}
+	finishTimeoutInt, _ := strconv.Atoi(finishTimeout)
+	time.Sleep(time.Duration(finishTimeoutInt) * time.Second)
 	_ = saramaProducer.Close()
 }
 
